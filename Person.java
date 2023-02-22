2b45221089481033e6cff5b4e683e5115316a3ac
lhs-devloper <sdsd0114@naver.com>
2023-02-22 16:20:32 +0900

    attack Method 구현!

--- Person.java
+++ Person.java
@@ -3,4 +3,8 @@ public class Person{
     public Person(String name){
         this.name = name;
     }
+
+    static void attack(Person person){
+        System.out.println(person.name + "님을 공격하였습니다");
+    }
 }
